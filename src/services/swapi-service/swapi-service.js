f234ec508291bd750d4cc74191af40ce3ecfd518
kalnynsh <kda869@yandex.ru>
2020-04-18 21:02:55 +0300

    Updated data transformation in SwapiService.

--- src/services/swapi-service/swapi-service.js
+++ src/services/swapi-service/swapi-service.js
@@ -97,8 +97,8 @@ export default class SwapiService {
             id: this._extractId(person),
             name: person.name,
             gender: person.gender,
-            birthYear: person.birthYeard,
-            eyeColor: person.eyeColor
+            birthYear: person.birth_year,
+            eyeColor: person.eye_color
         };
     };
 
@@ -108,11 +108,11 @@ export default class SwapiService {
             name: starship.name,
             model: starship.model,
             manufacturer: starship.manufacturer,
-            costInCredits: starship.costInCredits,
+            costInCredits: starship.cost_in_credits,
             length: starship.length,
             crew: starship.crew,
             passengers: starship.passengers,
-            cargoCapacity: starship.cargoCapacity,
+            cargoCapacity: starship.cargo_capacity,
         };
     };
 }
