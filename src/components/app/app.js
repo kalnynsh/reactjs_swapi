97aa0824577222d507d9c456dc384e6c994435f2
kalnynsh <kda869@yandex.ru>
2020-04-30 20:21:31 +0300

    Added feature handle not existing route to app.

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -11,8 +11,13 @@ import { SwapiServiceProvider } from '../swapi-service-context';
 
 import './app.css';
 
-import { BrowserRouter as Router, Route } from 'react-router-dom';
 import {StarshipDetails} from '../sw-components';
+import {
+    BrowserRouter as Router,
+    Route,
+    Switch,
+    Redirect,
+} from 'react-router-dom';
 
 import {
     PeoplePage,
@@ -77,42 +82,54 @@ export default class App extends Component {
 
                             <RandomPlanet />
 
-                            <Route
-                                path="/"
-                                render={() => <h2>Welcome to star wars app!</h2>}
-                                exact
-                            />
-                            <Route path="/people/:id?" component={PeoplePage} />
-                            <Route path="/planets" component={PlanetsPage} />
-                            <Route path="/starships" exact component={StarshipsPage} />
-                            <Route path="/starships/:id"
-                                render={({ match }) => {
-                                    const { id } = match.params;
-                                    return <StarshipDetails itemId={id} />;
-                                }}
-                            />
-                            <Route
-                                path="/login"
-                                render={() => (
-                                    <LoginPage
-                                        isLoggedIn={isLoggedIn}
-                                        onLogin={this.onLoginHandler}
-                                    />
-                                )}
-                            />
-                            <Route
-                                path="/logout"
-                                onClick={this.onLogoutHandler}
-                                render={() => (
-                                    <LogoutPage onLogout={this.onLogoutHandler} />
-                                )}
-                            />
-                            <Route
-                                path="/secret"
-                                render={() => (
-                                    <SecretPage isLoggedIn={isLoggedIn} />
-                                )}
-                            />
+                            <Switch>
+                                <Route
+                                    path="/"
+                                    render={() => <h2>Welcome to star wars app!</h2>}
+                                    exact
+                                />
+                                <Route path="/people/:id?" component={PeoplePage} />
+                                <Route path="/planets" component={PlanetsPage} />
+                                <Route path="/starships" exact component={StarshipsPage} />
+
+                                <Route path="/starships/:id"
+                                    render={({ match }) => {
+                                        const { id } = match.params;
+
+                                        if (Number.isInteger(+id)) {
+                                            return <StarshipDetails itemId={id} />;
+                                        }
+
+                                        return <Redirect to="/starships" />;
+                                    }}
+                                />
+
+                                <Route
+                                    path="/login"
+                                    render={() => (
+                                        <LoginPage
+                                            isLoggedIn={isLoggedIn}
+                                            onLogin={this.onLoginHandler}
+                                        />
+                                    )}
+                                />
+                                <Route
+                                    path="/logout"
+                                    onClick={this.onLogoutHandler}
+                                    render={() => (
+                                        <LogoutPage onLogout={this.onLogoutHandler} />
+                                    )}
+                                />
+                                <Route
+                                    path="/secret"
+                                    render={() => (
+                                        <SecretPage isLoggedIn={isLoggedIn} />
+                                    )}
+                                />
+                                <Route
+                                    render={() => <h2>Page not found</h2>}
+                                />
+                            </Switch>
 
                         </div>
                     </Router>
